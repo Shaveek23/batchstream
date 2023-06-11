fc1c4cf4b80dd5ce349e90f3b3de825e8bec39c1
Paweł Golik <01142171@pw.edu.pl>
2023-06-11 13:19:47 +0200

    main_elec2

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=stream
-#SBATCH --cpus-per-task=15
+#SBATCH --cpus-per-task=5
 #SBATCH --mail-type=END,FAIL
 #SBATCH --gpus-per-node=0
 #SBATCH --mail-user=01142171@pw.edu.pl
-#SBATCH --mem=12G
+#SBATCH --mem=5G
 #SBATCH -A stream-mining
 
 source /home2/faculty/pgolik/.pyenv/versions/batchstream/bin/activate
 
-python /home2/faculty/pgolik/batchstream/batchstream/main_led.py
+python /home2/faculty/pgolik/batchstream/batchstream/main_elec.py
 
