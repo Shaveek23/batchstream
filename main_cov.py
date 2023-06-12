507bc037f18cd0d5cac98467e16ffb55466f4057
Paweł Golik <01142171@pw.edu.pl>
2023-06-12 10:53:56 +0200

    cov combine

--- main_cov.py
+++ main_cov.py
@@ -46,16 +46,16 @@ def compose_experiments(dataset_name):
     is_performance_drift = True
 
     # LOGISTIC REGRESSION
-    lr = Pipeline([('sc', SC()), ('lr_batch', LogisticRegression())])
-    args_list.append((get_evidently_experiment(f"{suffix}_lr", lr, n_online=n_online, n_first_fit=n_first_fit, window_size=window_size,
-      n_curr=n_curr_ref_retrain, data_stattest_threshold=threshold,
-      data_drift=is_data_drift, target_drift=is_target_drift, is_performance=is_performance_drift), df.copy(deep=True)))
+    #lr = Pipeline([('sc', SC()), ('lr_batch', LogisticRegression())])
+    # args_list.append((get_evidently_experiment(f"{suffix}_lr", lr, n_online=n_online, n_first_fit=n_first_fit, window_size=window_size,
+    #   n_curr=n_curr_ref_retrain, data_stattest_threshold=threshold,
+    #   data_drift=is_data_drift, target_drift=is_target_drift, is_performance=is_performance_drift), df.copy(deep=True)))
     
     # DECISION TREE CLASSIFIER
     dt = Pipeline([('dt', DecisionTreeClassifier())])
-    args_list.append((get_evidently_experiment(f"{suffix}_dt", dt, n_online=n_online, n_first_fit=n_first_fit, window_size=window_size,
-      n_curr=n_curr_ref_retrain, data_stattest_threshold=threshold,
-      data_drift=is_data_drift, target_drift=is_target_drift, is_performance=is_performance_drift), df.copy(deep=True)))
+    # args_list.append((get_evidently_experiment(f"{suffix}_dt", dt, n_online=n_online, n_first_fit=n_first_fit, window_size=window_size,
+    #   n_curr=n_curr_ref_retrain, data_stattest_threshold=threshold,
+    #   data_drift=is_data_drift, target_drift=is_target_drift, is_performance=is_performance_drift), df.copy(deep=True)))
     
     # # NAIVE BAYES
     #nb = Pipeline([('nb_batch', GaussianNB())])
@@ -70,7 +70,7 @@ def compose_experiments(dataset_name):
     #   data_drift=is_data_drift, target_drift=is_target_drift, is_performance=is_performance_drift), df.copy(deep=True)))
     
     ##
-    #rf = Pipeline([('rf', RandomForestClassifier(random_state=42))])
+    rf = Pipeline([('rf', RandomForestClassifier(random_state=42))])
     #args_list.append((get_evidently_experiment(f"{suffix}_rf", rf, n_online=n_online, n_first_fit=n_first_fit, window_size=window_size,
     #  n_curr=n_curr_ref_retrain, data_stattest_threshold=threshold,
     #  data_drift=is_data_drift, target_drift=is_target_drift, is_performance=is_performance_drift), df.copy(deep=True)))
@@ -80,7 +80,7 @@ def compose_experiments(dataset_name):
     # suffix = f"online_{suffix_}"
 
     # # # # ADAPTIVE RANDOM FOREST
-    # arf = forest.ARFClassifier(seed=seed, leaf_prediction="mc")
+    arf = forest.ARFClassifier(seed=seed, leaf_prediction="mc")
     # # args_list.append((get_online_experiment(suffix, arf, window_size),  df.copy(deep=True)))
 
     # # # LOGISTIC REGRESSION
@@ -107,13 +107,13 @@ def compose_experiments(dataset_name):
     # # args_list.append((get_online_experiment(suffix, lr_online, window_size),  df.copy(deep=True)))
 
     # # # # HOEFFDING TREE 
-    # # hat = HoeffdingAdaptiveTreeClassifier(
-    # #     grace_period=100,
-    # #     delta=0.01,
-    # #     leaf_prediction='nb',
-    # #     nb_threshold=10,
-    # #     seed=seed
-    # # )
+    hat = HoeffdingAdaptiveTreeClassifier(
+        grace_period=100,
+        delta=0.01,
+        leaf_prediction='nb',
+        nb_threshold=10,
+        seed=seed
+    )
     # # args_list.append((get_online_experiment(suffix, hat.clone(), window_size),  df.copy(deep=True)))
 
     # # # # NAIVE BAYES
@@ -121,13 +121,13 @@ def compose_experiments(dataset_name):
     # # args_list.append((get_online_experiment(suffix, nb_online, window_size),  df.copy(deep=True)))
    
     # # # # SRP
-    # nominal_attributes = []
+    nominal_attributes = []
     # # if 'elec' in dataset_name.lower(): nominal_attributes = ["day_2", "day_3", "day_4", "day_5", "day_6", "day_7"]
     # # if 'stagger' in dataset_name.lower(): nominal_attributes = ['size_0', 'size_1', 'color_0', 'color_1', 'shape_0', 'shape_1']
 
-    # base_model = HoeffdingTreeClassifier(grace_period=100, delta=0.01, nominal_attributes=nominal_attributes)
-    # srp_model = ensemble.SRPClassifier(model=base_model, n_models=3, seed=seed)
-    # #args_list.append((get_online_experiment(suffix, srp_model, window_size),  df.copy(deep=True)))
+    base_model = HoeffdingTreeClassifier(grace_period=100, delta=0.01, nominal_attributes=nominal_attributes)
+    srp_model = ensemble.SRPClassifier(model=base_model, n_models=3, seed=seed)
+    #args_list.append((get_online_experiment(suffix, srp_model, window_size),  df.copy(deep=True)))
 
     # # # # No Change
     # # nc_online = dummy.NoChangeClassifier()
@@ -144,17 +144,17 @@ def compose_experiments(dataset_name):
     # #                                             [copy.deepcopy(lr_online), copy.deepcopy(arf)], window_size, n_curr_ref_retrain, threshold,
     # #      threshold, n_first_fit, n_online, is_data_drift=is_data_drift, is_target_drift=is_target_drift, is_performance=is_performance_drift, comb_type='ds'), df.copy(deep=True)))
     
-    # args_list.append((get_combining_exp_different_detectors(suffix + "_DS7", [copy.deepcopy(dt)], [copy.deepcopy(lr_online), copy.deepcopy(nb_online)], window_size, n_curr_ref_retrain,
-    #     n_first_fit, n_online, [0.01, 0.02], [0.01, 0.02], True, comb_type='ds'), df.copy(deep=True)))
+    args_list.append((get_combining_exp_different_detectors(suffix + "_DS8", [copy.deepcopy(dt)], [copy.deepcopy(hat), copy.deepcopy(arf)], window_size, n_curr_ref_retrain,
+        n_first_fit, n_online, [0.01, 0.02], [0.01, 0.02], True, comb_type='ds'), df.copy(deep=True)))
     
-    # args_list.append((get_combining_exp_different_detectors(suffix + "_MV7", [copy.deepcopy(dt)], [copy.deepcopy(lr_online), copy.deepcopy(nb_online)], window_size, n_curr_ref_retrain,
-    #     n_first_fit, n_online, [0.01, 0.02], [0.01, 0.02], True, comb_type='mv'), df.copy(deep=True)))
+    args_list.append((get_combining_exp_different_detectors(suffix + "_MV8", [copy.deepcopy(dt)], [copy.deepcopy(hat), copy.deepcopy(arf)], window_size, n_curr_ref_retrain,
+        n_first_fit, n_online, [0.01, 0.02], [0.01, 0.02], True, comb_type='mv'), df.copy(deep=True)))
 
-    # args_list.append((get_combining_experiment(suffix + "_MV6", [copy.deepcopy(rf)], [copy.deepcopy(srp_model), copy.deepcopy(arf)], window_size, n_curr_ref_retrain, threshold,
-    #     threshold, n_first_fit, n_online, is_data_drift=is_data_drift, is_target_drift=is_target_drift, is_performance=is_performance_drift, comb_type='mv'), df.copy(deep=True)))
+    args_list.append((get_combining_experiment(suffix + "_MV9", [copy.deepcopy(rf)], [copy.deepcopy(srp_model), copy.deepcopy(hat)], window_size, n_curr_ref_retrain, threshold,
+        threshold, n_first_fit, n_online, is_data_drift=is_data_drift, is_target_drift=is_target_drift, is_performance=is_performance_drift, comb_type='mv'), df.copy(deep=True)))
     
-    # args_list.append((get_combining_experiment(suffix + "_DS6", [copy.deepcopy(rf)], [copy.deepcopy(srp_model), copy.deepcopy(arf)], window_size, n_curr_ref_retrain, threshold,
-    #     threshold, n_first_fit, n_online, is_data_drift=is_data_drift, is_target_drift=is_target_drift, is_performance=is_performance_drift, comb_type='ds'), df.copy(deep=True)))
+    args_list.append((get_combining_experiment(suffix + "_DS9", [copy.deepcopy(rf)], [copy.deepcopy(srp_model), copy.deepcopy(hat)], window_size, n_curr_ref_retrain, threshold,
+        threshold, n_first_fit, n_online, is_data_drift=is_data_drift, is_target_drift=is_target_drift, is_performance=is_performance_drift, comb_type='ds'), df.copy(deep=True)))
 
     return args_list
 
