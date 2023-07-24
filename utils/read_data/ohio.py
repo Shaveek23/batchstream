ff21028c76a5384b372bbe7b8e2f9c58b4a7bce0
Paweł Golik <01142171@pw.edu.pl>
2023-07-24 22:45:33 +0200

    ohio dataset name fix

--- utils/read_data/ohio.py
+++ utils/read_data/ohio.py
@@ -6,4 +6,6 @@ from os import path
 
 def get_ohio_dataset(data_path='./data') -> Tuple[pd.DataFrame, pd.Series]:
     data_path = path.join(data_path, r'OHIO/ohio_merged.csv' )
-    return pd.read_csv(data_path, sep=';')
+    df = pd.read_csv(data_path, sep=';')
+    df['dataset'] = 'OHIO_merged'
+    return df
