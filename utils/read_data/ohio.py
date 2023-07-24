223398ffe253380e54c511a1a1e03d4c0f73a294
Paweł Golik <01142171@pw.edu.pl>
2023-07-24 22:40:10 +0200

    ohio data path fix

--- utils/read_data/ohio.py
+++ utils/read_data/ohio.py
@@ -5,5 +5,5 @@ from os import path
 
 
 def get_ohio_dataset(data_path='./data') -> Tuple[pd.DataFrame, pd.Series]:
-    data_path = path.join(data_path, r'/OHIO/ohio_merged.csv' )
+    data_path = path.join(data_path, r'OHIO/ohio_merged.csv' )
     return pd.read_csv(data_path, sep=';')
