2e08cc3df56aaa81cbfd1480aa149b75cc1d38ae
Paweł Golik <01142171@pw.edu.pl>
2023-08-07 21:48:45 +0200

    nhts read data fix

--- utils/read_data/nhts.py
+++ utils/read_data/nhts.py
@@ -7,5 +7,6 @@ from os import path
 def get_nhts_dataset(state_group_type, data_path='./data'):
     data_path = path.join(data_path, rf'NHTS/NHTS_{state_group_type}_merged.parquet.gzip')
     df = pd.read_parquet(data_path) 
+    df = df.reset_index(drop=True)
     df['dataset'] = f'NHTS_{state_group_type}'
     return df
