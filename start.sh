08b8816a03a2fd4d044bdeedbffb1b0af6696345
Paweł Golik <01142171@pw.edu.pl>
2023-08-19 11:45:50 +0200

    adding main_ohio

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=stream
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=12
 #SBATCH --mail-type=END,FAIL
 #SBATCH --gpus-per-node=0
 #SBATCH --mail-user=01142171@pw.edu.pl
-#SBATCH --mem=5G
+#SBATCH --mem=30G
 #SBATCH -A stream-mining
 
 source /home2/faculty/pgolik/.pyenv/versions/batchstream/bin/activate
 
-python /home2/faculty/pgolik/batchstream/batchstream/main_cov.py
+python /home2/faculty/pgolik/batchstream/main_ohio.py
 
